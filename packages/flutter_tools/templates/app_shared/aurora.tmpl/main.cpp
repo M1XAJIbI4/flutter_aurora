d6d62018340d7b0060727cf868e88be0507da8ae
Denis Glazkov <d.glazkov@omp.ru>
2023-06-08 11:41:03 +0300

    Use default pixel ratio in aurora applications

--- packages/flutter_tools/templates/app_shared/aurora.tmpl/main.cpp
+++ packages/flutter_tools/templates/app_shared/aurora.tmpl/main.cpp
@@ -3,7 +3,6 @@
 
 int main(int argc, char *argv[]) {
     Application::Initialize(argc, argv);
-    Application::SetPixelRatio(1.8);
     RegisterPlugins();
     Application::Launch();
     return 0;
